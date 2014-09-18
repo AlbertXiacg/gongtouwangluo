7ba04c922c6a012a087616099a07e4892f2079ea
appale2 <appale2@appale2deMac-mini.local>
2014-09-18 15:17:23 +0800

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by appale2 on 14-9-18.
+//  Copyright (c) 2014年 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by appale2 on 14-9-18.
+//  Copyright (c) 2014年 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
